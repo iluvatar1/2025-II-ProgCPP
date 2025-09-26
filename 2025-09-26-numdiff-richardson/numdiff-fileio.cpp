ebd20ddf35aa3ef8f7b48adcec068f3c6717bc1d
iluvatar1 <woquendo@gmail.com>
2025-09-26 08:00:55 -0500

    How to print to a file

---
+++ 2025-09-26-numdiff-richardson/numdiff-fileio.cpp
@@ -0,0 +1,62 @@
+#include <cmath>
+#include <print>
+#include <fstream> // para imprimir a archivo
+
+double fun(double x);
+double forward_diff(double x, double h);
+double central_diff(double x, double h);
+double rforward_diff(double x, double h);
+double rcentral_diff(double x, double h);
+
+
+int main()
+{
+    double x = 1.2345;
+    double exacto = std::cos(x);
+
+    // crear un file stream
+    std::ofstream fout("data.txt");
+
+    for (double h = 1.0e-16; h <= 1.0e-1; h = h*10.0) {
+        double xf = std::fabs(1 - forward_diff(x, h)/exacto);
+        double xc = std::fabs(1 - central_diff(x, h)/exacto);
+        double xrf = std::fabs(1 - rforward_diff(x, h)/exacto);
+        double xrc = std::fabs(1 - rcentral_diff(x, h)/exacto);
+        std::println(fout, "{:25.16e} {:25.16e} {:25.16e} {:25.16e} {:25.16e}", h, xf, xc, xrf, xrc);
+    }
+
+    fout.close(); // cerrar el stream
+
+    return 0;
+}
+
+double fun(double x)
+{
+    return std::sin(x);
+}
+
+double forward_diff(double x, double h)
+{
+    return (fun(x+h) - fun(x))/h;
+}
+
+double central_diff(double x, double h)
+{
+    return (fun(x+h) - fun(x-h))/(2*h);
+}
+
+double rforward_diff(double x, double h)
+{
+    double aux1 = forward_diff(x, h/2);
+    double aux2 = forward_diff(x, h);
+    double aux3 = std::pow(2.0, 1.0);
+    return (aux3*aux1 - aux2)/(aux3-1);
+}
+
+double rcentral_diff(double x, double h)
+{
+    double aux1 = central_diff(x, h/2);
+    double aux2 = central_diff(x, h);
+    double aux3 = std::pow(2.0, 2.0);
+    return (aux3*aux1 - aux2)/(aux3-1);
+}

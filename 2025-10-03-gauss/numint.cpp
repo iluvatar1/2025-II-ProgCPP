14adbfae2fd02971dc11e2d5ed8f8bc214ad94f3
iluvatar1 <woquendo@gmail.com>
2025-10-03 07:48:12 -0500

    Added basis for gauss2

---
+++ 2025-10-03-gauss/numint.cpp
@@ -0,0 +1,97 @@
+// Implementar trapecio y simpson y comparar errores
+// Se usara la funcion error para comparar
+
+#include <functional>
+#include <cmath>
+#include <print>
+#include <fstream>
+
+using fptr = std::function<double(double)>; // f(x)
+using algptr = std::function<double(double, double, int, fptr)>; // representa a forward_diff o central_diff
+
+double f(double x);
+double trapecio(double a, double b, int nintervals, fptr fun);
+double simpson(double a, double b, int nintervals, fptr fun);
+double richardson_int(double a, double b, int nsteps, 
+                      int order, algptr alg, fptr fun);
+double gauss2(double a, double b, fptr fun);
+
+int main(void)
+{
+    double exact = std::pow(std::sin(0.5), 2);
+    double a = 0.0;
+    double b = 1.0;
+
+    std::ofstream fout("data.txt");
+
+    int n = 2;
+    double et = std::fabs(1.0 - trapecio(a, b, n, f)/exact );
+    double es = std::fabs(1.0 - simpson(a, b, n, f)/exact );
+    double etr = std::fabs(1.0 - richardson_int(a, b, n, 2, trapecio, f)/exact );
+    double ets = std::fabs(1.0 - richardson_int(a, b, n, 4, simpson, f)/exact );
+    double etg2 = std::fabs(1.0 - gauss2(a, b, f)/exact );
+    std::println(fout, "{:10} {:25.16e} {:25.16e} {:25.16e} {:25.16e}", 
+                n, et, es, etr, ets);
+    fout.close();
+
+    return 0;
+}
+
+
+double f(double x)
+{
+    return x*std::sin(x*x);
+}
+
+
+double trapecio(double a, double b, int nintervals, fptr fun)
+{
+    double deltax = (b-a)/nintervals;
+    double suma = 0.0;
+    for(int k = 1; k <= nintervals - 1; k++) {
+        double xk = a + k*deltax;
+        suma = suma + fun(xk);
+    }
+    suma = suma + fun(a)/2 + fun(b)/2;
+
+    return deltax*suma;
+}
+
+double simpson(double a, double b, int nintervals, fptr fun)
+{
+    if (nintervals % 2 != 0) {
+        nintervals += 1;
+    }
+
+    double deltax = (b-a)/nintervals;
+
+    double suma1 = 0.0;
+    for(int k = 1; k <= nintervals/2; k++) {
+        double xk = a + (2*k-1)*deltax;
+        suma1 = suma1 + fun(xk);
+    }
+
+    double suma2 = 0.0;
+    for(int k = 1; k <= nintervals/2-1; k++) {
+        double xk = a + (2*k)*deltax;
+        suma2 = suma2 + fun(xk);
+    }
+
+    double suma = fun(a) + 4*suma1 + 2*suma2 + fun(b);
+
+    return deltax*suma/3;
+}
+
+double richardson_int(double a, double b, int nsteps, 
+                      int order, algptr alg, fptr fun)
+{
+    double aux1 = alg(a, b, 2*nsteps, fun);
+    double aux2 = alg(a, b, nsteps, fun);
+    double aux3 = std::pow(2.0, order);
+    return (aux3*aux1 - aux2)/(aux3-1);
+}
+
+double gauss2(double a, double b, fptr fun)
+{
+    return 1;
+}

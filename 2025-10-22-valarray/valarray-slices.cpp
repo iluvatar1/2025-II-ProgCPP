49c8855f8839fd58f39f5f2a4bc040c7282eeb14
iluvatar1 <woquendo@gmail.com>
2025-10-22 08:27:43 -0500

    Slices, masks examples, with experiments for failure

---
+++ 2025-10-22-valarray/valarray-slices.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <iostream>
+#include <valarray>
+#include <iomanip>
+//#include <print>
+
+void print(std::string msg, const std::valarray<int>& v)
+{
+    std::cout << msg << "\n";
+    for (int n: v) { std::cout << std::setw(3) << n; }
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::valarray<int> v1(3);
+    v1 = -1; // (3) from a scalar
+    print("assigned from scalar: ", v1);
+
+    v1 = {1, 2, 3, 4, 5, 6}; // (8): from initializer list of different size
+    print("assigned from initializer_list:", v1);
+
+    std::valarray<int> v2(3);
+    v2 = v1[std::slice(5,3,-2)]; // (4): from slice array
+    print("every 2nd element starting at pos 0:", v2);
+
+    std::valarray<int> v3 = v1[v1 >= 5 and v1 < 6]; // (6): from mask array
+    print("values that are even:", v3);
+
+    std::valarray<std::size_t> idx = {0,1,2,4}; // index array
+    v2.resize(4); // sizes must match when assigning from gen subscript
+    v2 = v1[idx]; // (7): from indirect array
+    print("values at positions 0, 1, 2, 4:", v2);
+
+    return 0;
+}

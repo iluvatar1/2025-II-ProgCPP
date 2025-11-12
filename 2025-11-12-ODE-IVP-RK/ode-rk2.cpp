e31ef5e68f294482aa4cd53c05e03cbe3c280c93
iluvatar1 <woquendo@gmail.com>
2025-11-12 08:20:39 -0500

    Added example for runge kutta 2

---
+++ 2025-11-12-ODE-IVP-RK/ode-rk2.cpp
@@ -0,0 +1,53 @@
+#include <valarray>
+#include <print>
+#include <map>
+#include <string>
+
+using state_t = std::valarray<double>;
+
+void fderiv(double t, const state_t  & s, state_t & dsdt, 
+            std::map<std::string, double> & p);
+void solve_ivp(double t0, double tf, double dt, state_t & s, 
+                std::map<std::string, double> & p);
+
+int main(void)
+{
+    state_t s(2);
+    // initial conditions
+    s[0] = 1.2345;
+    s[1] = 0.0;
+    // params
+    std::map<std::string, double> params;
+    params["W0"] = 2.435;
+
+    state_t dsdt(2);
+    solve_ivp(0.0, 10.987765, 0.1, s, params);
+
+    return 0;
+}
+
+void fderiv(double t, const state_t  & s, state_t & dsdt, 
+            std::map<std::string, double> & p)
+{
+    dsdt[0] = s[1];
+    dsdt[1] = -p["W0"]*p["W0"]*s[0];
+}
+
+void solve_ivp(double t0, double tf, double dt, state_t & s, std::map<std::string, double> & p)
+{
+    state_t dsdt(s.size()), k1(s.size()), k2(s.size());
+    for(double t = t0; t <= tf; t = t + dt) {
+        // fderiv(t, s, dsdt, p);
+        // s = s + dt*dsdt;// euler
+
+        // rk2
+        fderiv(t, s, dsdt, p);
+        k1 = dt*dsdt;
+        fderiv(t + dt, s + k1, dsdt, p);
+        k2 = dt*dsdt;
+        s = s + 0.5*(k1 + k2);
+
+        // print
+        std::println("{} {} {}", t, s[0], s[1]);
+    }
+}

e49038b8aecc93e45e422b233b6f7a89cd520a86
iluvatar1 <woquendo@gmail.com>
2025-11-19 07:54:41 -0500

    Added loop overe time

---
+++ 2025-11-19-MD/md-v2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <valarray>
+#include <vector>
+#include <map>
+#include <string>
+
+// one bouncing particle 
+
+struct Particle {
+    std::valarray<double> R{0.0, 0.0, 0.0}, V{0.0, 0.0, 0.0}, F{0.0, 0.0, 0.0};
+    double mass{0}, rad{0};
+    void print(void);
+};
+
+void initial_conditions(std::vector<Particle> & bodies);
+
+int main(void)
+{
+    int N = 1;
+    std::vector<Particle> particles(N);
+
+    // params
+    std::map<std::string, double> params;
+    params["T0"] = 0.0;
+    params["TF"] = 10.0;
+    params["DT"] = 0.1;
+
+
+    // initial conditions
+    initial_conditions(particles);
+
+    // loop over time
+    for (double t = params["T0"]; t <= params["TF"]; t = t + params["DT"]) {
+        //   compute forces
+        //   time step integration
+        //   constrains
+        //   print info
+        particles[0].print();
+        std::cout << "\n";
+    }
+
+    return 0;
+}
+
+// particle.cpp
+void Particle::print(void) {
+    std::cout << mass << "\t" << rad << "\t"
+              << R[0] << "\t" << R[1] << "\t" << R[2] << "\t"
+              << V[0] << "\t" << V[1] << "\t" << V[2] << "\t"
+              << F[0] << "\t" << F[1] << "\t" << F[2] << "\t";
+}
+
+void initial_conditions(std::vector<Particle> & bodies)
+{
+    bodies[0].R[0] = 1.2345;
+    bodies[0].R[1] = 2.9345;
+    bodies[0].rad  = 0.2345;
+    bodies[0].mass = 1.7345;
+}
